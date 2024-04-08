73072992cf29c9f686763f0ac7ff7af3166e57c8
Noemi Millman <noemi@triopter.com>
2024-04-08 13:08:00 -0400

    chatgpt rename, as usual needs work

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -15,7 +15,7 @@
 """
 Via https://xxyxyz.org/line-breaking/:
 
-The method using dynamic programming can be written as two nested loops: 
+The method using "dynamic programming" can be written as two nested loops: 
 the outer one iterates over every word and the inner one searches for the most 
 suitable break. Hirschberg and Larmore showed in 1987 an algorithm which is 
 able to use binary search instead of the inner loop in the case the weight 
@@ -24,7 +24,8 @@ made to satisfy this property by handling the "infinities" slightly differently,
 giving an overall time of O(n * log n). A similar algorithm due to Galil and 
 Giancarlo from 1989[1] is given below.
 
-The concave property says: w(i, j) + w(i', j') <= w(i', j) + w(i, j'), where i < i' < j < j'.
+The concave property says: w(i, j) + w(i', j') <= w(i', j) + w(i, j'), 
+where i < i' < j < j'.
 
 [1] Z. Galil, R. Giancarlo. Speeding up dynamic programming with applications 
 to molecular biology. Theoretical Computer Science 64, 1989.
@@ -36,56 +37,79 @@ from collections import deque
 
 def break_lines(text, max_length):
     words = text.split()
-    count = len(words)
-    offsets = [0]
-    for w in words:
-        offsets.append(offsets[-1] + len(w))
+    word_count = len(words)
+    word_offsets = [0]
+    for word in words:
+        word_offsets.append(word_offsets[-1] + len(word))
 
-    minima = [0] * (count + 1)
-    breaks = [0] * (count + 1)
+    line_minima = [0] * (word_count + 1)
+    line_breaks = [0] * (word_count + 1)
 
-    def c(i, j):
-        w = offsets[j] - offsets[i] + j - i - 1
-        if w > max_length:
-            return 10**10 * (w - max_length)
-        return minima[i] + (max_length - w) ** 2
+    def calculate_penalty(start_index, end_index):
+        line_width = (
+            word_offsets[end_index]
+            - word_offsets[start_index]
+            + end_index
+            - start_index
+            - 1
+        )
+        if line_width > max_length:
+            return 10**10 * (line_width - max_length)
+        return line_minima[start_index] + (max_length - line_width) ** 2
 
-    def h(l, k):
-        low, high = l + 1, count
-        while low < high:
-            mid = (low + high) // 2
-            if c(l, mid) <= c(k, mid):
-                high = mid
+    def find_best_break(previous_break, current_break):
+        lower_bound, upper_bound = previous_break + 1, word_count
+        while lower_bound < upper_bound:
+            middle = (lower_bound + upper_bound) // 2
+            if calculate_penalty(previous_break, middle) <= calculate_penalty(
+                current_break, middle
+            ):
+                upper_bound = middle
             else:
-                low = mid + 1
-        if c(l, high) <= c(k, high):
-            return high
-        return l + 2
+                lower_bound = middle + 1
+        if calculate_penalty(previous_break, upper_bound) <= calculate_penalty(
+            current_break, upper_bound
+        ):
+            return upper_bound
+        return previous_break + 2
 
-    q = deque([(0, 1)])
-    for j in range(1, count + 1):
-        l = q[0][0]
-        if c(j - 1, j) <= c(l, j):
-            minima[j] = c(j - 1, j)
-            breaks[j] = j - 1
-            q.clear()
-            q.append((j - 1, j + 1))
+    break_queue = deque([(0, 1)])
+    for current_index in range(1, word_count + 1):
+        previous_break = break_queue[0][0]
+        if calculate_penalty(current_index - 1, current_index) <= calculate_penalty(
+            previous_break, current_index
+        ):
+            line_minima[current_index] = calculate_penalty(
+                current_index - 1, current_index
+            )
+            line_breaks[current_index] = current_index - 1
+            break_queue.clear()
+            break_queue.append((current_index - 1, current_index + 1))
         else:
-            minima[j] = c(l, j)
-            breaks[j] = l
-            while c(j - 1, q[-1][1]) <= c(q[-1][0], q[-1][1]):
-                q.pop()
-            q.append((j - 1, h(j - 1, q[-1][0])))
-            if j + 1 == q[1][1]:
-                q.popleft()
+            line_minima[current_index] = calculate_penalty(
+                previous_break, current_index
+            )
+            line_breaks[current_index] = previous_break
+            while calculate_penalty(
+                current_index - 1, break_queue[-1][1]
+            ) <= calculate_penalty(break_queue[-1][0], break_queue[-1][1]):
+                break_queue.pop()
+            break_queue.append(
+                (
+                    current_index - 1,
+                    find_best_break(current_index - 1, break_queue[-1][0]),
+                )
+            )
+            if current_index + 1 == break_queue[1][1]:
+                break_queue.popleft()
             else:
-                q[0] = q[0][0], (q[0][1] + 1)
+                break_queue[0] = break_queue[0][0], (break_queue[0][1] + 1)
 
-    lines = []
-    j = count
-    while j > 0:
-        i = breaks[j]
-        lines.append(" ".join(words[i:j]))
-        j = i
-    lines.reverse()
-    return lines
+    formatted_lines = []
+    current_index = word_count
+    while current_index > 0:
+        start_index = line_breaks[current_index]
+        formatted_lines.append(" ".join(words[start_index:current_index]))
+        current_index = start_index
+    formatted_lines.reverse()
+    return formatted_lines
