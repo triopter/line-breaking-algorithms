07785ffe5d648c789726cd71c77cc818c943f093
Noemi Millman <noemi@triopter.com>
2024-04-09 17:28:18 -0400

    randomization in tests

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -26,9 +26,30 @@ def make_test_id(case):
     return case.name
 
 
+def calculate_penalty(lines, max_length):
+    return sum([(max_length - len(line)) ** 2 for line in lines])
+
+
 @pytest.mark.parametrize("in_len_out", INPUTS_LENGTHS_OUTPUTS, ids=make_test_id)
 def test_correctness(algorithm, in_len_out):
-    if algorithm:
-        assert algorithm.break_lines(
-            in_len_out.input, in_len_out.max_line_length
-        ) == in_len_out.output.split("\n")
+    if not algorithm:
+        return
+
+    # don't test our algorithms against inputs that are too long for them to perform reasonably
+    if algorithm.break_lines.max_test_length < len(in_len_out.input.split()):
+        return
+
+    print(
+        algorithm.__file__,
+        in_len_out,
+        "\\n".join(algorithm.break_lines(in_len_out.input, in_len_out.max_line_length)),
+    )
+
+    lines = algorithm.break_lines(in_len_out.input, in_len_out.max_line_length)
+    expected = in_len_out.output.split("\n")
+
+    assert calculate_penalty(lines, in_len_out.max_line_length) == calculate_penalty(
+        expected, in_len_out.max_line_length
+    )
+
+    # assert lines == expected
